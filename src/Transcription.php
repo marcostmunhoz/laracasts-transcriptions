ef8b0c9fe6e04bfcc474bc99a005bf8cc46f3690
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2022-05-09 00:15:36 -0300

    feat(transcription): Discard irrelevant (header, empty and number only) lines on load

--- src/Transcription.php
+++ src/Transcription.php
@@ -11,18 +11,46 @@ class Transcription
     {
         $instance = new static();
 
-        $instance->lines = file($path);
+        $instance->lines = $instance->discardIrrelevantLines(file($path));
 
         return $instance;
     }
 
     public function __toString(): string
     {
-        return implode('', $this->lines);
+        return implode("\n", $this->lines);
     }
 
     public function lines(): array
     {
         return $this->lines;
+    }
+
+    /**
+     * @param string[] $lines
+     * 
+     * @return string[]
+     */
+    protected function discardIrrelevantLines(array $lines): array
+    {
+        return array_values(
+            array_filter( 
+                array_map('trim', $lines),
+                function (string $line): bool {
+                    // ignores WEBVTT header
+                    if ($line === 'WEBVTT') {
+                        return false;
+                    }
+
+                    // ignores lines with only numbers
+                    if (is_numeric($line)) {
+                        return false;
+                    }
+
+                    // ignores empty lines
+                    return (bool) $line;
+                }
+            )
+        );
     }
 }
